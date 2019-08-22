6370f1dc0ba353162a0847780a33504cf0b9bbb7
Sang-Hyun Kim <shkim7876@naver.com>
2019-08-22 17:50:25 +0900

    [add]unit module
    캐릭터와 그 캐릭터에 해당하는 UI를 하나로 묶기 위해서 제작했다. 아직은 불안정하니 아니다 싶으면 날릴 거임.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import draw
 import UI
 from game_project import Character, Near_Enemy, GROUND_HEIGHT
 from collision import collide_list_to_list
-
+import unit
 
 '''
 800*600 크기. 카메라 무브 없이 한 맵에서 1:1 내지는 1:3 정도의 전투를 한다.
@@ -23,24 +23,30 @@ pygame.display.set_caption("연습")
 painter = draw.painter(DISP)
 painter.append_bg('image/MapSunny.png')
 
+units = {'enemy':[], 'items':[]} #각각의 유닛을 종류별로 리스트에 담고, 리스트를 하나의 딕셔너리에 넣어 관리.
+                                 #플레이어는 단 하나의 값만 가지므로 리스트에 쓰기는 부담스럽고, 따라서 units딕셔너리엔 넣지 않았다.
 
-player = Character()
-playerHPbar = UI.Character_stat(player, (5, 5))
+#트리 형태로 보면 다음과 같다.
+#units(Dict)->enemy(list)->nearenemySet->Near_Enemy
+#                                       ->UI
+#           ->items(list)->itemSet(아직까진 아이템을 쓸 지도 미확정이지만.)
+
+player = unit.characterSet(Character()) #캐릭터와 그 캐릭터에 해당하는 인터페이스까지 한번에 생성한다.
+units['enemy'].append(unit.nearenemySet(Near_Enemy()))
+units['enemy'].append(unit.nearenemySet(Near_Enemy(position=(550,GROUND_HEIGHT))))
 painter.append(player)
-painter.append(playerHPbar)
 
-enemy_1 = Near_Enemy()
-enemyHPbar = UI.Enemy_stat(enemy_1, (0, 180))
-painter.append(enemy_1)
-painter.append(enemyHPbar)
+for items in units.values(): #units['player'], units['enemy'], units['items']
+    for item in items: #각각의 리스트에 있는 플레이어들, 적들, 아이템들을 불러온다.
+        painter.append(item) #unit.unitSet을 통째로 등록한다.
 
-enemy_2 = Near_Enemy(position=(550, GROUND_HEIGHT))
-enemy2HPbar = UI.Enemy_stat(enemy_2, (0, 180))
-painter.append(enemy_2)
-painter.append(enemy2HPbar)
 
-hitbox_layer = {'player':[player.hitbox],            'enemy':[enemy_1.hitbox],          'item': [],
-                'player_attack':[player.atk_hitbox], 'enemy_attack':[enemy_1.atk_hitbox], 'throwable':[]
+hitbox_layer = {'player':       [player.character.hitbox],
+                'player_attack':[player.character.atk_hitbox],
+                'enemy' :       [i.character.hitbox for i in units['enemy']],
+                'enemy_attack': [i.character.atk_hitbox  for i in units['enemy']],
+                'item':         [],
+                'throwable':    []
                }
 
 WHITE = (255, 255, 255)
@@ -64,10 +70,11 @@ while running:
    
     player.control(pygame.key.get_pressed())
     player.update()
-    enemy_1.near_ai(player)
-    enemy_1.update()
-    enemy_2.near_ai(player)
-    enemy_2.update()
+
+    for enemy in units['enemy']:
+        enemy.ai(player.character)
+        enemy.update()
+
     painter.image_update()
     painter.draw()
 
---
+++ unit.py
@@ -0,0 +1,38 @@
+﻿import game_project
+import UI
+import draw
+from abc import *
+
+class unitSet(draw.drawable, metaclass=ABCMeta): #지금은 하는 것은 없다. 유닛 하나와 관련된 것을 다루는 클래스임을 명시.
+    def draw(self, surf):
+        self.character.draw(surf)
+        self.UI.draw(surf)
+
+    def image_update(self):
+        self.character.image_update()
+        self.UI.image_update()
+
+
+class characterSet(unitSet): #캐릭터와 캐릭터 UI등 하나의 캐릭터와 관련된 모든 것을 포함, 관리하는 클래스.
+    def __init__(self, character): #캐릭터와 UI 속성 등을 넣어주면 그걸 토대로 클래스를 초기화한다. UI생성 방법은 모두 동일하니 고정.
+        assert isinstance(character, game_project.Character) #character에는 Character 인스턴스만 허용.
+        self.character = character
+        self.UI = UI.Character_stat(self.character, pos=(5, 5)) #UI는 알아서 생성한다.
+
+    def update(self):
+        return self.character.update()
+
+    def control(self, keys):
+        return self.character.control(keys)
+
+class nearenemySet(unitSet): #근거리 공격 적과 그 UI등 하나의 근거리 적과 관련된 모든 것을 포함, 관리하는 클래스.
+    def __init__(self, enemy): #적과 UI 속성 등을 넣어주면 그걸 토대로 클래스를 초기화한다. UI생성방법은 모두 동일하니 고정.
+        assert isinstance(enemy, game_project.Near_Enemy)
+        self.character = enemy
+        self.UI = UI.Enemy_stat(self.character, pos=(0, 180))
+    
+    def ai(self, other):
+        return self.character.near_ai(other)
+
+    def update(self):
+        return self.character.update()
